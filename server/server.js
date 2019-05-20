780438d42bdeb91d89c2bb1d226ed59491bff3c5
Michael <michi.moritz@gmail.com>
2019-05-20 19:54:59 +0200

    basic REST backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,38 @@
+const mysql = require("mysql");
 const express = require("express");
-const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const app = express();
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "M160195m",
+  database: "eventservice"
 });
 
-app.listen(3000, () => console.log("listening on port 3000..."));
+app.get("/events", (req, res) => {
+  connection.connect();
+  connection.query("SELECT * FROM event;", function(err, rows, fields) {
+    if (err) throw err;
+    console.log("The solution is: ", rows[0]);
+    res.send("Success!");
+  });
+  connection.end();
+  //res.send("Hello World");
+});
+
+app.get("/events/:id", (req, res) => {
+  connection.connect();
+  connection.query(`SELECT * FROM event WHERE id=${req.params.id};`, function(
+    err,
+    rows,
+    fields
+  ) {
+    if (err) throw err;
+    console.log("The solution is: ", rows[0]);
+    res.send(rows[0]);
+  });
+  connection.end();
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on port ${port}...`));
